c0605c204321abcfc8bc7663229d6c12e5ea1ac1
AAnuj26 <aanujbhagat2828@gmail.com>
2023-12-11 15:07:04 +0530

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
      
      <h1>Hello</h1>
+     <h2><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos saepe porro minus suscipit necessitatibus ullam esse consequuntur. Soluta aut ex mollitia vitae ullam minima laboriosam, reprehenderit repellat, unde ipsum aspernatur.</p></h2>
     </>
   )
 }
