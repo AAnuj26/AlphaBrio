ec108f5dd9a5788aa1233c6039e4c2ecf42723dc
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-01-27 16:34:06 +0530

    feat / contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="contact-us" element={<Contact />} />
           <Route path="store" element={<OurStore />} />
           <Route path="blogs" element={<Blog />} />
         </Route>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,212 +1,98 @@
 import React from "react";
 import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import { FaHome } from "react-icons/fa";
+import { BiPhoneCall, BiLogoGmail, BiInfoCircle } from "react-icons/bi";
 
 const Contact = () => {
   return (
     <>
-      <Meta title={"Contact"} />
-      <div className="mb-4 mx-4 my-4 mt-4">
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readOnly
-              className="htmlForm-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label
-            htmlFor="inputPassword"
-            className="col-sm-2 col-htmlForm-label"
-          >
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="htmlForm-control"
-              id="inputPassword"
-            />
+      <Meta title={"Contact Us"} />
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.5759753024477!2d73.07495980000002!3d19.08237195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c1c2c6e3b45b%3A0x5663747380a7768f!2sMhada%20Colony%2C%20Sector%2037%2C%20Navi%20Mumbai%2C%20Maharashtra%20410210!5e0!3m2!1sen!2sin!4v1706342526866!5m2!1sen!2sin"
+                width="600"
+                height="450"
+                className="border-0 w-100"
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-betweem">
+                <div>
+                  <h3 className="contact-title">Contact</h3>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
+                    </div>
+                  </form>
+                </div>
+                <div>
+                  <h3 className="contact-title">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <FaHome className="fs-5" />
+                        <address className="mb-0">
+                          1234, Main Road, Near City Hall, New Panvel
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+91 9234738306">+91 9234738306</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiLogoGmail className="fs-5" />
+                        <a href="email:alpha9234@gmail.com">
+                          alpha9234@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
