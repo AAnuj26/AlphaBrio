31face2517eee559cd71afd66f9911637f2fc2ca
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-01-27 12:59:22 +0530

    Feat/Blog page Made initial blog page to display blogs using blog card component made earlier

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,183 +7,204 @@ const Contact = () => {
       <Meta title={"Contact"} />
       <div className="mb-4 mx-4 my-4 mt-4">
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              className="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              className="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
-              id="inputPassword"
-            />
-          </div>
-        </div>{" "}
-        <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              readonly
-              className="form-control-plaintext"
-              id="staticEmail"
-              value="email@example.com"
-            />
-          </div>
-        </div>
-        <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
-            Password
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="password"
-              className="form-control"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
         </div>{" "}
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              className="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
         </div>{" "}
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              className="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
         </div>{" "}
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              className="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
         </div>{" "}
         <div className="mb-3 row">
-          <label for="staticEmail" className="col-sm-2 col-form-label">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
-              class="form-control-plaintext"
+              readOnly
+              className="htmlForm-control-plaintext"
               id="staticEmail"
               value="email@example.com"
             />
           </div>
         </div>
         <div className="mb-3 row">
-          <label for="inputPassword" className="col-sm-2 col-form-label">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
             Password
           </label>
           <div className="col-sm-10">
             <input
               type="password"
-              className="form-control"
+              className="htmlForm-control"
+              id="inputPassword"
+            />
+          </div>
+        </div>{" "}
+        <div className="mb-3 row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-htmlForm-label">
+            Email
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              readOnly
+              className="htmlForm-control-plaintext"
+              id="staticEmail"
+              value="email@example.com"
+            />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label
+            htmlFor="inputPassword"
+            className="col-sm-2 col-htmlForm-label"
+          >
+            Password
+          </label>
+          <div className="col-sm-10">
+            <input
+              type="password"
+              className="htmlForm-control"
               id="inputPassword"
             />
           </div>
