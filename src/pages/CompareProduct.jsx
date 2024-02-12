20035ddd72fb2ed9b2aa1f90c3509004ffc022fb
Anuj Ravindra Bhagat <bhagatanuj2828@gmail.com>
2024-02-12 23:53:47 +0530

    Feat / compare products page

--- src/pages/CompareProduct.jsx
+++ src/pages/CompareProduct.jsx
@@ -1,6 +1,6 @@
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-
+import Colors from "../components/Colors";
 const CompareProduct = () => {
   return (
     <>
@@ -27,7 +27,81 @@ const CompareProduct = () => {
                   <h5 className="title">
                     Honor T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+4G Tablet
                   </h5>
-                  <h6 className="price">₹10000</h6>
+                  <h6 className="price mb-3">₹10000</h6>
+                  <div>
+                    <div className="product-detail">
+                      <h5>Brand:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Type:</h5>
+                      <p>Watch</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Availability:</h5>
+                      <p>In Stock</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Color:</h5>
+                      <Colors />
+                    </div>
+                    <div className="product-detail">
+                      <h5>Size:</h5>
+                      <div className="d-flex gap-10">
+                        <p>S</p>
+                        <p>M</p>
+                        <p>L</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="compare-product-card position-relative">
+                <img
+                  src="images/cross.svg"
+                  alt="cross"
+                  className="position-absolute cross img-fluid"
+                />
+                <div className="product-card-image">
+                  <img
+                    src="images/watch.jpg"
+                    alt="watch"
+                    className="img-fluid"
+                  />
+                </div>
+                <div className="compare-product-details">
+                  <h5 className="title">
+                    Honor T1 7.0 1 GB RAM 8 GB ROM 7 Inch With Wi-Fi+4G Tablet
+                  </h5>
+                  <h6 className="price mb-3">₹10000</h6>
+                  <div>
+                    <div className="product-detail">
+                      <h5>Brand:</h5>
+                      <p>Havels</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Type:</h5>
+                      <p>Watch</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Availability:</h5>
+                      <p>In Stock</p>
+                    </div>
+                    <div className="product-detail">
+                      <h5>Color:</h5>
+                      <Colors />
+                    </div>
+                    <div className="product-detail">
+                      <h5>Size:</h5>
+                      <div className="d-flex gap-10">
+                        <p>S</p>
+                        <p>M</p>
+                        <p>L</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
