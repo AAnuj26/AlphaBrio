f4ac83fc566ed5b8960e97b7debcaa81cce3dfd0
AAnuj26 <bhagatanuj2828@gmail.com>
2024-09-02 15:22:12 +0530

    Refactor SingleProduct page and update review section

--- src/client/src/pages/SingleProduct.jsx
+++ src/client/src/pages/SingleProduct.jsx
@@ -24,8 +24,8 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                <h4>Description</h4>
                 <p className="bg-white p-3">
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Quibusdam, similique minus, ipsam numquam praesentium
@@ -42,28 +42,102 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="review-head d-flex justify-content-between align-items-end">
-                <div>
-                  <h4 className="mb-2">Customer Reviews</h4>
-                  <div className="d-flex gap-10 justify-content-center">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p>Based on 2 Reviews</p>
+              <h3>Reviews</h3>
+              <div className="review-inner-wrapper ">
+                <div className="review-head d-flex justify-content-between align-items-end">
+                  <div>
+                    <h4 className="mb-2">Customer Reviews</h4>
+                    <div className="d-flex gap-10 justify-content-center">
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p>Based on 2 Reviews</p>
+                    </div>
+                  </div>
+                  {orderedProduct ? (
+                    <div>
+                      <a
+                        className="text-dark text-decoration-underline"
+                        href=""
+                      >
+                        {" "}
+                        Write a Review
+                      </a>
+                    </div>
+                  ) : null}
+                </div>
+                <div className="review-form py-4">
+                  <h4 className="mb-2">Write A Review</h4>
+
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={true}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="reviews">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Anuj</h6>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Aspernatur repellat quia nostrum laboriosam ducimus
+                      quibusdam perferendis. Nihil fugit libero quis
+                      voluptatibus, accusamus dolorem, reiciendis nobis eligendi
+                      nesciunt, assumenda molestias rerum.
+                    </p>
+                  </div>
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Arun</h6>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Aspernatur repellat quia nostrum laboriosam ducimus
+                      quibusdam perferendis. Nihil fugit libero quis
+                      voluptatibus, accusamus dolorem, reiciendis nobis eligendi
+                      nesciunt, assumenda molestias rerum.
+                    </p>
                   </div>
                 </div>
-                {orderedProduct ? (
-                  <div>
-                    <a className="text-dark text-decoration-underline" href="">
-                      {" "}
-                      Write a Review
-                    </a>
-                  </div>
-                ) : null}
               </div>
             </div>
           </div>
